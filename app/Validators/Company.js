cb36cce383b423a231ff9e6a619c8a82722ad58d
rayleshh <melqui.pereira@celsolisboa.edu.br>
2019-12-26 11:43:10 -0300

    feat: Implementado a tradução do erros de validação
    Implementado validação condicional de banco de dados

--- app/Validators/Company.js
+++ app/Validators/Company.js
@@ -1,9 +1,19 @@
+const Antl = use('Antl');
+
 class Company {
+	get validateAll() {
+		return true;
+	}
+
 	get rules() {
 		return {
 			name: 'required',
 			cnpj: 'required',
 		};
+	}
+
+	get messages() {
+		return Antl.list('validation');
 	}
 }
 
