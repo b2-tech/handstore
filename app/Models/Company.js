520d3e25021315fea3736dfb3a82dc936f6f0f18
rayleshh <melqui.pereira@celsolisboa.edu.br>
2019-12-26 11:43:37 -0300

    Ajustado o relacionamento com a tabela usuarios

--- app/Models/Company.js
+++ app/Models/Company.js
@@ -1,6 +1,10 @@
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model');
 
-class Company extends Model {}
+class Company extends Model {
+	users() {
+		return this.hasMany('App/Models/User');
+	}
+}
 
 module.exports = Company;
